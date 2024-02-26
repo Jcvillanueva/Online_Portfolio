e145559ad59ae727882a8f1dbfca9ac105033b01
John Paul Villanueva <Jcvillanueva@student.apc.edu.ph>
2024-02-26 16:45:20 +0800

    updated header and homepage

--- my-web-portfolio/src/app/app-routing.module.ts
+++ my-web-portfolio/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { HeaderComponent } from './pages/header/header.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'Homepage', component: HomepageComponent },
+  { path: 'Header', component: HeaderComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
