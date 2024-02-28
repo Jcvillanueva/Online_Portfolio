d22912c45a7cff3a86fd159ff133aa5dac8099dd
John Paul Villanueva <Jcvillanueva@student.apc.edu.ph>
2024-02-28 10:42:48 +0800

    Revert "updated header and homepage"

--- my-web-portfolio/src/app/app-routing.module.ts
+++ my-web-portfolio/src/app/app-routing.module.ts
@@ -1,12 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomepageComponent } from './pages/homepage/homepage.component';
-import { HeaderComponent } from './pages/header/header.component';
 
-const routes: Routes = [
-  { path: 'Homepage', component: HomepageComponent },
-  { path: 'Header', component: HeaderComponent },
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
