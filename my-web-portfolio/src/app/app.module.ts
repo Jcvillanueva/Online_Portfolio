c97f7232c05bd2e7cd820ac2119afa5282b7cbba
John Paul Villanueva <Jcvillanueva@student.apc.edu.ph>
2024-03-04 14:21:19 +0800

    Updated angular material
    Practicing my Container skills coz I prolly forgot how to do them but hey new progress? right?

--- my-web-portfolio/src/app/app.module.ts
+++ my-web-portfolio/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarHeaderComponent } from './components/navbar-header/navbar-header.component';
 import { HomepageComponent } from './pages/homepage/homepage.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatGridListModule} from '@angular/material/grid-list';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import { HomeBannerComponent } from './components/home-banner/home-banner.component';
@@ -24,7 +25,8 @@ import { HomeBannerComponent } from './components/home-banner/home-banner.compon
     BrowserAnimationsModule,
     MatButtonModule,
     MatIconModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- my-web-portfolio/src/app/components/home-banner/home-banner.component.ts
+++ my-web-portfolio/src/app/components/home-banner/home-banner.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-home-banner',
   templateUrl: './home-banner.component.html',
